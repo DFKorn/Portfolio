6653e1dee533d42596ca0cd8506f0735d6dc3662
DFKorn <d1man2192@mail.ru>
2022-09-26 20:02:47 +0300

    base changes

---
+++ content.js
@@ -0,0 +1,42 @@
+const content = {
+    en:{
+        navigaion:{
+            hero: 'Home',
+            about: 'About',
+            projects: 'Projects'
+        },
+        hero:{
+            h1: 'Dmitry Kornatovskiy ',
+            p: 'Front-End web developer'
+        },
+        about: {
+            h2: 'About Me',
+            textBox:"Hi, my name is Dmitry Kornatovskiy. I tend to be a front-end developer. I don't have commerce experince so far but i have completed a few projects which you can see below. This site is my personal web portfolio. Here I gathered projects that show my skills."
+        },
+        recenWorks:{
+            h2: 'Recent Works',
+            p: 'Here gathered my latest projects'
+        }
+    },
+    ru:{
+        navigaion:{
+            hero: 'Домой',
+            about: 'Обо мне',
+            projects: 'Мои проекты'
+        },
+        hero:{
+            h1: 'Дмитрий Корнатовский',
+            p: 'Front-End web developer'
+        },
+        about: {
+            h2: 'Обо мне',
+            textBox:"Привет, меня зову Дмитрий Корнатовский. Я недавно закончил курсы обучения по напрвлению фронтенд-разработчик.Это сайт - мое портфолио. Здесь собраны проекты демонстрирющие мои навыки."
+        },
+        recenWorks:{
+            h2: 'Мои проекты',
+            p: 'Здесь собраны мои последние проекты'
+        }
+    }
+}
+
+export default content;
