fc744781faff3a143a196f9e2899c0947e22a31c
DFKorn <d1man2192@mail.ru>
2022-10-07 14:04:49 +0300

    test toggleMenu

--- main.js
+++ main.js
@@ -34,48 +34,6 @@ const langSwitch = () => {
 langToggleButton.addEventListener('click', langSwitch)
 
 
-
-// const content = {
-//     en:{
-//         navigation:{
-//             home: 'Home',
-//             about: 'About',
-//             projects: 'Projects'
-//         },
-//         hero:{
-//             h1: 'Dmitry Kornatovskiy ',
-//             p: 'Front-End web developer'
-//         },
-//         about: {
-//             h2: 'About Me',
-//             p:"Hi, my name is Dmitry Kornatovskiy. I tend to be a front-end developer. I don't have commerce experince so far but i have completed a few projects which you can see below. This site is my personal web portfolio. Here I gathered projects that show my skills."
-//         },
-//         recentWorks:{
-//             h2: 'Recent Works',
-//             p: 'Here gathered my latest projects'
-//         }
-//     },
-//     ru:{
-//         navigation:{
-//             home: 'Домой',
-//             about: 'Обо мне',
-//             projects: 'Мои проекты'
-//         },
-//         hero:{
-//             h1: 'Дмитрий Корнатовский',
-//             p: 'Front-End web developer'
-//         },
-//         about: {
-//             h2: 'Обо мне',
-//             p:"Привет, меня зову Дмитрий Корнатовский. Я недавно закончил курсы обучения по напрвлению фронтенд-разработчик.Это сайт - мое портфолио. Здесь собраны проекты демонстрирющие мои навыки."
-//         },
-//         recentWorks:{
-//             h2: 'Мои проекты',
-//             p: 'Здесь собраны мои последние проекты'
-//         }
-//     }
-// }
-
 let navHome = document.querySelector('.nav-home');
 let navAbout = document.querySelector('.nav-about');
 let navProjects = document.querySelector('.nav-projects');
@@ -178,10 +136,23 @@ themeSwitch.onclick = function(){
 }
 
 //Navigation menu
+
+navigation.onclick = toggleMenu;
+main.onclick = toggleMenu;
+topbar.onclick = toggleMenu;
+
+
+
+// function toggleMenu(){
+//     let navigation = document.querySelector('nav');
+//     let main = document.querySelector('main');
+//     let topbar = document.querySelector('.topbar');
+//     navigation.classList.remove('active');
+//     main.classList.remove('active');
+//     topbar.classList.remove('active')
+// }
+
 function toggleMenu(){
-    let navigation = document.querySelector('nav');
-    let main = document.querySelector('main');
-    let topbar = document.querySelector('.topbar');
     navigation.classList.remove('active');
     main.classList.remove('active');
     topbar.classList.remove('active')
